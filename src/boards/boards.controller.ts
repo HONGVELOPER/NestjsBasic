200aefac500468c1dacdd3d454080d340bda0948
Young-Jin1003 <hyjinn1003@naver.com>
2022-01-27 18:05:44 +0900

    api test(crud) with postgresql

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,21 +1,65 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Board } from './board.model';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	ParseIntPipe,
+	Patch,
+	Post,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
 	constructor(private boardsService: BoardsService) {}
 
 	@Get('/')
-	getAllBoard(): Board[] {
+	getAllBoards(): Promise<Board[]> {
 		return this.boardsService.getAllBoards();
 	}
 
 	@Post()
-	createBoard(
-		@Body('title') title: string,
-		@Body('description') description: string,
-	): Board {
-		return this.boardsService.createBoard(title, description);
+	@UsePipes(ValidationPipe)
+	createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+		return this.boardsService.createBoard(createBoardDto);
 	}
+
+	// localhost:3000/id=dalsdsada
+	@Get('/:id')
+	getBoardById(@Param('id') id: number): Promise<Board> {
+		return this.boardsService.getBoardById(id);
+	}
+
+	@Delete('/:id')
+	deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
+		return this.boardsService.deleteBoard(id);
+	}
+
+	@Patch('/:id/status')
+	updateBoardStatus(
+		@Param('id', ParseIntPipe) id: number,
+		@Body('status', BoardStatusValidationPipe) status: BoardStatus,
+	): Promise<Board> {
+		return this.boardsService.updateBoardStatus(id, status);
+	}
+
+	// @Delete('/:id')
+	// deleteBoard(@Param('id') id: string): void {
+	// 	this.boardsService.deleteBoard(id);
+	// }
+
+	// @Patch('/:id/status')
+	// updateBoardStatus(
+	// 	@Param('id') id: string,
+	// 	@Body('status', BoardStatusValidationPipe) status: BoardStatus,
+	// ): Board {
+	// 	return this.boardsService.updateBoardStatus(id, status);
+	// }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,23 +1,65 @@
 import { Injectable } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
-import { v1 as uuid } from 'uuid';
+import { BoardStatus } from './board-status.enum';
+// import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
-	private boards: Board[] = [];
+	constructor(
+		@InjectRepository(BoardRepository)
+		private boardRepository: BoardRepository,
+	) {}
 
-	getAllBoards(): Board[] {
-		return this.boards;
+	createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+		return this.boardRepository.createBoard(createBoardDto);
 	}
 
-	createBoard(title: string, description: string) {
-		const board: Board = {
-			id: uuid(),
-			title, // title: title 입력하는 것과 같다.
-			description,
-			status: BoardStatus.PUBLIC,
-		};
-		this.boards.push(board);
-		return board;
+	getBoardById(id: number): Promise<Board> {
+		return this.boardRepository.getBoardById(id);
 	}
+
+	getAllBoards(): Promise<Board[]> {
+		return this.boardRepository.getAllBoards();
+	}
+
+	deleteBoard(id: number): Promise<void> {
+		return this.boardRepository.deleteBoard(id);
+	}
+
+	updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+		return this.boardRepository.updateBoardStatus(id, status);
+	}
+	// getAllBoards(): Board[] {
+	// 	return this.boards;
+	// }
+	// createBoard(createBoardDto: CreateBoardDto) {
+	// 	const { title, description } = createBoardDto;
+	// 	const board: Board = {
+	// 		id: uuid(),
+	// 		title, // title: title 입력하는 것과 같다.
+	// 		description,
+	// 		status: BoardStatus.PUBLIC,
+	// 	};
+	// 	this.boards.push(board);
+	// 	return board;
+	// }
+	// getBoardById(id: string): Board {
+	// 	const found = this.boards.find((board) => board.id === id);
+	// 	if (!found) {
+	// 		throw new NotFoundException(`Can't find Board with id: ${id}`);
+	// 	}
+	// 	return found;
+	// }
+	// deleteBoard(id: string): void {
+	// 	const found = this.getBoardById(id); // getBoardById에 이미 예외처리 존재
+	// 	this.boards = this.boards.filter((board) => board.id !== found.id);
+	// }
+	// updateBoardStatus(id: string, status: BoardStatus): Board {
+	// 	const board = this.getBoardById(id);
+	// 	board.status = status;
+	// 	return board;
+	// }
 }
